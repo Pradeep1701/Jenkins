95de9e1f047ed5c07457e4e8177f982b630c9e32
Pradeep1701 <pradeepr53109@gmail.com>
2025-03-08 14:05:55 +0530

    First Commit

---
+++ src/main/java/com/jenkins/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.jenkins.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+	
+	
+	@PostMapping("/display")
+	public void display() {
+		System.out.println("Service A is displaying");
+	}
+
+}
